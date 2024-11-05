d5dfd933e8d5ef700eb2dabcaf71a67277783b5e
Wyatt H <jhawes@ucsc.edu>
2024-11-05 01:51:19 -0800

    even more base

--- replicate.ts
+++ replicate.ts
@@ -3,11 +3,11 @@ const nodes = {}; // Maybe want a [] instead
 
 // When any key or node change happens, we loop through all nodes
 // and send an internal API call to all other nodes?
-function add_key(should_replicate: boolean) {}
+function add_key(key, value, should_replicate: boolean) {}
 
-function delete_key(should_replicate: boolean) {}
+function delete_key(key, should_replicate: boolean) {}
 
-function get_key(should_replicate: boolean) {}
+function get_key(key, should_replicate: boolean) {}
 
 function add_node(should_replicate: boolean) {}
 
