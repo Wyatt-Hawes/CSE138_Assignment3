7b4d8803072aae6c66a25dab3709ed3b71fe1c89
Wyatt H <wyatt.hawes2003@gmail.com>
2024-11-08 18:17:56 -0800

    blocking update request on same server. Adding simple version counter that increments (but not removed through delete)

--- http_server.go
+++ http_server.go
@@ -15,21 +15,24 @@ type js map[string]interface{}
 // Debug false disables all prints done with the log() function
 const debug bool = true
 var kv_pairs = make(js) // Creates an empty map for us to use
+var kv_version = make(js)
 
-// var view []string = strings.Split(os.Getenv("VIEW"),",");
+// var VIEW []string = strings.Split(os.Getenv("VIEW"),",");
 // Above works getting env, for testing, redefine
-var view = []string{"localhost:8090", "localhost:8091"}
+var VIEW = []string{"localhost:8090", "localhost:8091"}
 
+//var ip string = os.Getenv("IP");
+var IP = "localhost:8091"
 
 func main(){
 	http.HandleFunc("/kvs/", kvs_handler) // All method types go to each handler (GET POST PUT DELETE etc.)
 	http.HandleFunc("/update", update_handler)
 
-	fmt.Fprintln(os.Stdout,view);
+	fmt.Fprintln(os.Stdout,VIEW);
 	fmt.Fprintln(os.Stdout, "Server running!\n---------------")
 
 	// Change from 8090 to 8091 when doing scuffed replication testing (8090 -> launch 1 server, 8091 -> launch 2nd server)
-	http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":8091", nil)
 }
 
 func kvs_handler(w http.ResponseWriter, r *http.Request) {
@@ -75,7 +78,7 @@ func kvs_handler(w http.ResponseWriter, r *http.Request) {
 		// Replicate if successful
 		if (status == http.StatusCreated || status == http.StatusOK){
 			log("Replicating PUT")
-			go replicate("PUT", key, value_str); // Go launches a `goroutine` aka async call
+			go replicate("PUT", key, value_str, get_version(key)); // Go launches a `goroutine` aka async call
 		}
 
 		break
@@ -88,7 +91,7 @@ func kvs_handler(w http.ResponseWriter, r *http.Request) {
 		// Replicate if successful
 		if (status == http.StatusCreated || status == http.StatusOK){
 			log("Replicating DELETE")
-			go replicate("DELETE", key, ""); // Go launches a `goroutine` aka async call
+			go replicate("DELETE", key, "", get_version(key)); // Go launches a `goroutine` aka async call
 		}
 		break
 
@@ -118,14 +121,25 @@ func update_handler(w http.ResponseWriter, r *http.Request){
 	method_d, e1 := body["method"]
 	key_d, e2 := body["key"]
 	value_d, e3 := body["value"]
+	version_d, e7 := body["version"]
 
 	// Convert all to string
 	method, e4 := method_d.(string)
 	key, e5 := key_d.(string)
 	value, e6 := value_d.(string)
+	version_f, e8 := version_d.(float64)
+	version := int(version_f)
 
+	// log(body)
+	// log(fmt.Sprintf("%t %t %t %t %t %t %t %t",!e1, !e2, !e3, !e4, !e5, !e6, !e7, !e8))
+	// log(version)
+	// log(reflect.TypeOf(version))
+	// log(version_d)
+	// log(reflect.TypeOf(version_d))
+	// log(fmt.Sprintf("Type is %t", version))
 	// If any errors, drop request
-	if(!e1 || !e2 || !e3 || !e4 || !e5 || !e6){
+	if(!e1 || !e2 || !e3 || !e4  || !e5 || !e6 || !e7 || !e8){
+		log("Error with update")
 		return;
 	}
 
@@ -134,13 +148,16 @@ func update_handler(w http.ResponseWriter, r *http.Request){
 		case "PUT":
 			log("Received external Update for PUT")
 			put_key(key, value)
+			set_version(key, version)
 			break;
 		case "DELETE":
 			log("Received external Update for DELETE")
 			delete_key(key);
+			//set_version(key, version)
 			break;
 		default:
 			break;
 	}
 	w.WriteHeader(http.StatusOK);
+	return;
 }
--- key_value_ops.go
+++ key_value_ops.go
@@ -17,12 +17,16 @@ func get_key(key string) (js, int) {
 	// Check metadata version. If meta-data is GREATER THAN current version, invalid request(?)
 
 	value, exists := kv_pairs[key]
-
+	version := get_version(key)
 	if !exists {
-		return js{"error": "Key does not exist"}, http.StatusNotFound
+		res:= js{"error": "Key does not exist"}
+		res["casual-metadata"] = js{"key":key, "version":version}
+		if(version == 0){
+			res["casual-metadata"] = js{"key":key, "version":nil};
+		}
+		return res, http.StatusNotFound
 	}
-
-	return js{"result": value}, http.StatusOK
+	return js{"result": value, "casual-metadata":js{"key":key,"version":version}}, http.StatusOK
 }
 
 func put_key(key string, value string) (js, int) {
@@ -41,6 +45,10 @@ func put_key(key string, value string) (js, int) {
 	}
 
 	kv_pairs[key] = value
+
+	version := get_add_version(key)
+	res["casual-metadata"] = js{"key":key, "version": version}
+
 	return res, status
 }
 
@@ -54,26 +62,32 @@ func delete_key(key string) (js, int) {
 	}
 	
 	delete(kv_pairs, key)
+	res := js{"result": "deleted"}
 
-	return js{"result": "deleted"}, http.StatusOK
+	version := get_add_version(key)
+	res["casual-metadata"] = js{"key":key, "version": version}
+
+	return res, http.StatusOK
 }
 
-func replicate(method string, key string, value string){
+func replicate(method string, key string, value string, version int){
 
-	for _, v := range view{
+	for _, v := range VIEW{
 		/*
 			We should probably have some logic here to NOT communicate with ourselves, but 
 			the way it is right now, a request also gets sent to ourself, which has no effect ofc but is annoying
 		*/
+		if(v == IP){
+			continue;
+		}
 
 		// For each view, make a request to it with the update
-
 		// Form the full URL
 		url:= fmt.Sprintf("http://%s/update",v)
 		log(fmt.Sprintf("URL:%s",url))
 
 		// Form body with all needed info (for now)
-		body := js{"method":method,"key":key,"value":value}
+		body := js{"method":method,"key":key,"value":value, "version": version}
 		body_js, _ := json.Marshal(body)
 
 		// Create the request
@@ -93,4 +107,37 @@ func communicate(req *http.Request){
 	client.Do(req);
 
 	// Maybe check for errors here and add/remove servers from the VIEW
+}
+
+func get_add_version(key string)(version int){
+	vs_d, exists := kv_version[key];
+	vs, e := vs_d.(int)
+	// If no entry exists, create it with a version 1
+	if (!exists || !e){
+		log("Version doesnt exist")
+		kv_version[key] = 0
+		vs = 0
+	}
+
+	// Increment version
+	vs++
+
+	// Set new version & return
+	set_version(key, vs)
+	return vs
+}
+
+func get_version(key string)(version int){
+	v_d, _ := kv_version[key]
+
+	v, _ := v_d.(int)
+
+	return v;
+}
+
+//func get_version
+
+func set_version(key string, version int){
+	log("Setting: "+key+" to "+fmt.Sprint(version))
+	kv_version[key] = version;
 }
